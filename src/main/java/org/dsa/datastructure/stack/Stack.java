59e8bcf75a22e9486b00ee27af8d89dd3cb43af0
imfoobar42 <akashsingh@Akashs-MacBook-Air.local>
2024-07-02 17:28:27 +0530

    Added Stack DS

---
+++ src/main/java/org/dsa/datastructure/stack/Stack.java
@@ -0,0 +1,45 @@
+package org.dsa.datastructure.stack;
+
+//Implement Stack
+public class Stack {
+    private  Node top;
+    private int height;
+
+    class Node{
+
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Stack(int value){
+        Node newNode = new Node(value);
+        height = 1;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Node getTop() {
+        return top;
+    }
+    public void push(int value){
+        Node node = new Node(value);
+        if(top==null)  top = node;
+        else {
+            node.next = top;
+            top = node;
+        }
+        ++height;
+    }
+    public int pop(){
+        if(top==null) return 0;
+        Node temp = top;
+        top = top.next;
+        --height;
+        return temp.value;
+    }
+}
